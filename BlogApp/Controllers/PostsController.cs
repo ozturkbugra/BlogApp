3e01962398c2091119f9996f327076d5d6941992
ozturkbugra <bugra_ozturk1905@hotmail.com>
2025-05-12 21:25:30 +0300

    post ve user'a resim olayı eklendi bloglar basit şekilde listelendi.

---
+++ BlogApp/Controllers/PostsController.cs
@@ -0,0 +1,21 @@
+﻿using BlogApp.Data.Concrete.EFCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly BlogContext _context;
+
+        public PostsController(BlogContext context)
+        {
+            _context = context;
+        }
+
+       
+        public IActionResult Index()
+        {
+            return View(_context.Posts.ToList());
+        }
+    }
+}
--- BlogApp/Entity/User.cs
+++ BlogApp/Entity/User.cs
@@ -5,6 +5,9 @@
         public int UserID { get; set; }
         public string? UserName { get; set; }
 
+        public string? Image { get; set; }
+
+
         public List<Post> Posts { get; set; } = new List<Post>();
 
         public List<Comment> Comments { get; set; } = new List<Comment>();
